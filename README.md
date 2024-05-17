9749eead02debb8b76f10d384022dfa07efca5f2
melissemhm <melissa.mahmoudi.09@gmail.com>
2024-05-17 15:35:14 +0200

    modification de readme et ajout du script de scrapping et le corpus

--- README.md
+++ README.md
@@ -4,21 +4,21 @@
 
 > La tache à réaliser :
 
-Je propose de me concentrer sur la constitution d'un corpus français pour une tâche de classification de textes littéraires en fonction de leur genre (par exemple, roman policier, science-fiction, romance, etc.). 
+Je propose de me concentrer sur la constitution d'un corpus français pour une tâche de classification de textes littéraires en fonction de leur thèmes (par exemple: vengeance, drame, romance, etc.). 
 
 > Corpus :
 
 Le corpus qui m'a inspiré est **_BookCorpus_** https://huggingface.co/datasets/bookcorpus
 
-**Description du Corpus** : Ce corpus est une ressource massive de littérature gratuite composée de 11 038 livres en anglais, ce qui équivaut à environ 74 millions de phrases et 1 milliard de mots. Ces livres sont regroupés en 16 sous-genres tels que la romance, l'histoire, etc. Ils sont issus de sources originales, ce qui signifie qu'ils ne sont pas annotés et qu'ils représentent une diversité de styles d'écriture et de thèmes.
+**Description du Corpus** : Le dataset BookCorpus sur Hugging Face comprend une grande collection de textes issus de livres écrits par des auteurs non publiés, disponibles sur le site Smashwords. Il est composé de 11 038 livres en anglais, ce qui équivaut à environ 74 millions de phrases et 1 milliard de mots. Ces livres sont regroupés en 16 sous-genres tels que la romance, l'histoire, etc. Il soulève des questions éthiques, notamment sur la collecte des données sans consentement explicite des auteurs.
 
 **Tâches et sous tâches** : Le corpus BookCorpus peut être exploité pour plusieurs tâches dans le domaine du traitement du langage naturel, notamment la génération de texte et le remplissage de masque pour compléter des phrases manquantes. Ces tâches sont particulièrement utiles pour entraîner des modèles de langage et améliorer la compréhension du contexte par les modèles de traitement automatique du langage.
 
 
 >  À quel modèle il a servi :
 
-Le corpus a été utilisé pour entraîner une variété de modèles de traitement du langage naturel, en particulier pour la génération de texte et la modélisation de langage. Il a été utilisé pour l'entraînement de modèles tels que BERT (Bidirectional Encoder Representations from Transformers), GPT (Generative Pre-trained Transformer), RoBERTa (Robustly optimized BERT approach), entre autres.
+Le corpus a été utilisé pour l'entraînement de modèles tels que BERT (Bidirectional Encoder Representations from Transformers), GPT (Generative Pre-trained Transformer), RoBERTa (Robustly optimized BERT approach).
 
 > Corpus à constituer :
 
-Je vais constituer un corpus similaire en version française, je vais explorer d'autres sources de textes littéraires et effectuer une recherche approfondie pour trouver des textes variés et représentatifs. Une foie le corpus est constitué, je vais effectuez un prétraitement pour les nettoyer et les formater de manière cohérente puis annoter chaque texte avec son genre littéraire correspondant.
+Je vais constituer un corpus similaire en version française, je vais explorer d'autres sources de textes littéraires et effectuer une recherche approfondie pour trouver des textes représentatifs. Une foie le corpus est constitué, je vais effectuez un prétraitement pour les nettoyer et les formater de manière cohérente puis l'annoter.
---
+++ Scripts/scrapping.py
@@ -0,0 +1,72 @@
+import httpx 
+from lxml import html  
+import csv 
+import re  
+import sys  
+
+def get_urls(url):
+    """Cette fonction accède à un lien spécifique puis récuppère tous les liens qui y existe mais filtre seulement ceux qui concernent l'explication des textes en utilisant une regex."""
+    with httpx.Client(verify=False) as client:
+        response = client.get(url)
+        tree = html.fromstring(response.content)
+        # Extraire les liens contenant "/explications-textes/" suivis de deux chiffres à la fin
+        links = tree.xpath('//a[contains(@href, "/explications-textes/")]/@href')
+        filtered_links = [link for link in links if re.search(r'/explications-textes/\d{2}$', link)]
+        return filtered_links
+
+def extract_specific_links(url):
+    """Cette fonction extrait à parir des liens de la premère fonction des liens spécifiques qui contiennent les textes qu'on veut extraire."""
+    with httpx.Client(verify=False) as client:
+        response = client.get(url)
+        tree = html.fromstring(response.content)
+        # Extraire les liens à partir de l'emplacement spécifié avec ce chemin XPATH
+        specific_links = tree.xpath('/html/body/div[1]/main/div/div/div[1]/div/ul//a/@href')
+        return specific_links
+
+def extract_content(root):
+    """Cette fonction extrait le contenu texte des liens récupérés par la 2ème fonction à partir d'un élément racine donné."""
+    content = root.xpath("/html/body/div[1]/main/div/div/div[1]/div/blockquote")
+    #il ya des liens qui contiennent deux partie de blockquote (voire plus, et c'est difficile de mettre un chemin xpath général si y'en a plusieurs); sans cette conditoon, la fonction peut extraire un contenu autre que le texte voulu, c'est pourquoi, j'ai mis la condition d'ignorer les liens qui ont plus d'une partie  blockquote.
+    if len(content) == 1:
+        return content[0].text_content()
+    return None
+
+def write_to_csv(writer, text):
+    """Cette fonction écrit le texte dans un fichier CSV, chaque phrase étant une ligne."""
+    # Diviser le texte à chaque point et écrire chaque phrase comme une nouvelle ligne
+    sentences = text.split('.')
+    for sentence in sentences:
+        trimmed_sentence = sentence.strip()
+        if trimmed_sentence:  # s'assurer qu'aucune phrase vide n'est écrite
+            writer.writerow({'text': trimmed_sentence + '.'})
+
+def process_links_and_write_data(writer, urls):
+    """Cette fonction traite les liens et écrit les données extraites dans le fichier CSV."""
+    for url in urls:
+        print(f"Processing {url}")
+        specific_links = extract_specific_links(url)
+        for link in specific_links:
+            response = httpx.get(link)
+            content = extract_content(html.fromstring(response.content))
+            if content:
+                write_to_csv(writer, content)
+
+def main(output_file):
+    """Cette fonction coordonne le processus d'extraction et d'écriture."""
+    main_url = 'https://www.etudes-litteraires.com/explications-textes/'
+    initial_urls = get_urls(main_url)
+
+    with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['text']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        process_links_and_write_data(writer, initial_urls)
+
+if __name__ == "__main__":
+    # voici la structure de la commande
+    if len(sys.argv) != 2:
+        print("Usage: python scrapping.py <output_file>")
+        sys.exit(1)
+    output_file = sys.argv[1]
+    main(output_file)
